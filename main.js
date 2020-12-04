33fce1d05f039d818bf1a94bc32c99008e489417
David Stavis <davidstavis@gmail.com>
2020-12-04 12:30:42 -0700

    [wip] Completed game loop, and aces transform
    * Also fixed a bug in deck.deal that caused the final element in the deck not to be drawn
    *Ace value change is not working properly, diagnosing why

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ class Card{
 	constructor(face, suit){
 		this.face = face;
 		this.suit = suit;
+		this._value = 0;
 		this.value = face;
 		this.name = face + " of " + suit;
 	}
@@ -21,9 +22,11 @@ class Card{
 		}else if (typeof face === 'string'){
 			if (['Jack', 'Queen', 'King'].includes(face)){
 				this._value = 10;
-			}else if(face === 'Ace'){ // an ace can value at 1 or 11, players' choice (or, according to which state is most optimal)
-				this._value = 1;
+			}else if(face === 'Ace'){ // an ace can value at 1 or 11, according to which state is most optimal (11 if point total <=21, else 1)
+				this._value = 11;
 				// TODO: Make the Ace card automatically update its value to either 11 or 1, depending on which will get the owning player's total value closest to 21 without busting
+			}else if(face === "Ace*"){
+				this._value = 1;
 			}else{
 				console.log("Error: valid arguments include a number from 2-10, Ace, Jack, Queen, or King")//TODO: Make this actually throw an error
 			}
@@ -37,7 +40,7 @@ class Card{
 let deck = {
 	deal() {
 		// choose a card at random from the remaining cards
-		randomNumber = Math.floor(Math.random() * (this.remainingCards.length + 1))
+		randomNumber = Math.floor(Math.random() * (this.remainingCards.length))
 		randomCard = this.remainingCards[randomNumber];
 		// remove it from the deck
 		this._remove(randomNumber)
@@ -46,7 +49,7 @@ let deck = {
 	},
 	_remove(cardIndex) {
 		// remove a card from the deck
-		// should be called by , not directly
+		// should be called by deal, not directly
 		this.remainingCards.splice(cardIndex, 1)
 	},
 	remainingCards: [],
@@ -96,11 +99,27 @@ class Hand{
 			return pointsSoFar += currentCard.value;
 		}, 0);
 		
-		this.pointTotal = currentPointTotal;
+		if (currentPointTotal > 21){
+			// if your points would be over 21 but there are one or more aces in your hand whose values are 11, change the first ace's value to 1 and recalculate
+			let indexOfBigAce = this.getIndexOfBigAce();
+			if(indexOfBigAce !== -1){
+				// find first big ace and make it small
+				this.cards[indexOfBigAce].value = "Ace*";
+				this.updatePointTotal(); //becomes recursive
+			}
+		}
 
+		this.pointTotal = currentPointTotal;
 		if (this.pointTotal > 21){
 			this.player.bust();
 		}
+	}
+
+	getIndexOfBigAce(){
+		let ace = this.cards.findIndex( card=> {
+			return ((card.face === "Ace") && (card._value === 11))
+		})
+		return ace;
 	}
 };
 
@@ -120,6 +139,7 @@ class Player{
 
 	stand() {
 		// lets the gameManager know that they're going to end their turn. once the user stands, the dealer takes their turn, and then the winner is decided
+		gameManager.standHandler(this);	
 	}
 
 	bust() {
@@ -131,19 +151,49 @@ let gameManager = {
 	dealer: new Player("dealer"),
 	user: new Player("user"),
 	endGame(loser) {
+
+		// TODO: Disable user buttons until restart (and ideally make them look disabled)
+
 		let messageText = "";
 		// if the dealer lost, display a message indicating that the user won
 		if (loser === "dealer"){
 			messageText = "Congratulations! You win! Refresh the page to play a new round.";
-			console.log(messageText);
 		}else if (loser === "user"){
 			messageText = "Oooh, sorry...you lost this round. Refresh the page to play a new round.";
-			console.log(messageText);
 		}
 		
 		viewManager.displayMessage(messageText);
 
 		// display a reset button to start a new game
+	},
+	determineLoser(){
+		if (this.dealer.hand.pointTotal > this.user.hand.pointTotal){
+			this.endGame(this.user.id);
+		} else{
+			this.endGame(this.dealer.id);
+		}
+	},
+	standHandler(player){
+		if (player.id === "user"){
+			this.aiPlayerTurn();
+		}else if(player.id === "dealer"){
+			this.determineLoser()
+		}else{
+			console.log("Argument error: expected existing player id as a string")
+		}
+	},
+	aiPlayerTurn(){
+		let aiPlayer = this.dealer;
+		// If the dealer has <17 points, they hit. If they hit and bust, the game ends and the user wins.
+		// If they have 17-21 points, they stand. When the dealer stands, compare their score to the user's score:
+		// if the user's score is higher, they win. if the dealer's score is higher, the user loses.
+		if(aiPlayer.hand.pointTotal < 17){
+			aiPlayer.hit();
+			this.aiPlayerTurn();
+		}else if ((aiPlayer.hand.pointTotal >= 17) && (aiPlayer.hand.pointTotal <= 21)){
+			aiPlayer.stand();
+		}
+
 	},
 	startNewGame() {
 		// clear the visual cards off the screen TODO: implement visual cards and clear them
@@ -160,15 +210,20 @@ let gameManager = {
 		this.user.hit()
 		this.user.hit()
 
+		// Update the starting view
 		viewManager.updatePlayerViews(this.dealer)
 		viewManager.updatePlayerViews(this.user)
 
 		// Dealer pauses, and user can either hit or stand
 		// If the user hits, they may bust, which will end the game
 		// If they stand, the dealer takes their turn
-		// If the dealer has <17 points, they hit. If they hit and bust, the game ends and the user wins.
-		// If they have 17-21 points, they stand. When the dealer stands, compare their score to the user's score:
-		// if the user's score is higher, they win. if the dealer's score is higher, the user loses.
+		// Wait for user input
+		$('#userButtons .hit.button').on('click', function(){
+			gameManager.user.hit()
+		})
+		$('#userButtons .stand.button').on('click', function(){
+			gameManager.user.stand()
+		})
 	}
 }
 
@@ -191,8 +246,8 @@ let viewManager = {
 		let points = player.hand.pointTotal;
 		$(`.${playerName} .score`).text(`Points: ${points}`)
 	},
-	updatePlayerViews(player){
-		this.updateHandView(player);
+	updatePlayerViews(player, card){
+		this.updateHandView(player, card);
 		this.updatePointView(player);
 	},
 	addCardToHandView(player, card){
